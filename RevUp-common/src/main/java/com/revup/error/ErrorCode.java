ccc50a94bd3db28ba52da06987773b60313002da
jmd5314 <jmd5314@gmail.com>
2024-11-22 19:19:51 +0900

    fix: 에러 수정 (#12)

--- RevUp-common/src/main/java/com/revup/error/ErrorCode.java
+++ RevUp-common/src/main/java/com/revup/error/ErrorCode.java
@@ -32,7 +32,6 @@ public enum ErrorCode {
     OTHER_SERVER_FORBIDDEN(FORBIDDEN, "외부 api 403에러"),
 
     QUESTION_NOT_FOUND(HttpStatus.NOT_FOUND, "질문 id : %s 가 존재하지 않습니다"),
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "회원 id : %s 가 존재하지 않습니다"),
     FEEDBACK_NOT_FOUND(HttpStatus.NOT_FOUND, "피드백 id : %s 가 존재하지 않습니다"),
     FEEDBACK_CODE_NOT_FOUND(HttpStatus.NOT_FOUND, "피드백코드 id : %s 가 존재하지 않습니다"),
     INTERRUPTED(HttpStatus.SERVICE_UNAVAILABLE, "처리가 강제로 중단되었습니다. 잠시 후 다시 시도해주세요."),
