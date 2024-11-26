67785ca1137075398531a39602466fd335de883b
junho <>
2024-11-26 22:44:35 +0900

    refactor: #11 User 엔티티 구조 변경에 따른 수정

--- RevUp-api/src/main/java/com/revup/auth/service/LoginUseCase.java
+++ RevUp-api/src/main/java/com/revup/auth/service/LoginUseCase.java
@@ -7,8 +7,10 @@ import com.revup.auth.model.mapper.AuthMapper;
 import com.revup.user.entity.User;
 import com.revup.user.util.UserUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @UseCase
+@Slf4j
 @RequiredArgsConstructor
 public class LoginUseCase {
 
@@ -17,6 +19,7 @@ public class LoginUseCase {
 
     public FirstLoginResponse execute() {
         User user = userUtil.getCurrentUser();
-        return authMapper.toFirstLoginResponse(user.getNickname());
+        log.info("user = {}", user);
+        return authMapper.toFirstLoginResponse(user.getProfile());
     }
 }
