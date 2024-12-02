f5c5c6676afa962db2bfc085229c1d8caed847af
power-minu <mw0677@naver.com>
2024-12-02 22:19:52 +0900

    업적 초기데이터 삽입 복구

--- RevUp-domain/src/main/resources/sql/data.sql
+++ RevUp-domain/src/main/resources/sql/data.sql
@@ -1,39 +1,39 @@
-# -- 리뷰 요청 업적
-# INSERT IGNORE INTO achievements (created_at, updated_at, is_deleted, description, standard, type)
-# VALUES ('2024-11-27 00:00:00', '2024-11-27 00:00:00', 'FALSE', '', 10, 'REQUESTED_REVIEW');
-# INSERT IGNORE INTO achievements (created_at, updated_at, is_deleted, description, standard, type)
-# VALUES ('2024-11-27 00:00:00', '2024-11-27 00:00:00', 'FALSE', '', 20, 'REQUESTED_REVIEW');
-# INSERT IGNORE INTO achievements (created_at, updated_at, is_deleted, description, standard, type)
-# VALUES ('2024-11-27 00:00:00', '2024-11-27 00:00:00', 'FALSE', '', 30, 'REQUESTED_REVIEW');
-#
-# -- 누적 추천 수 업적
-# INSERT IGNORE INTO achievements (created_at, updated_at, is_deleted, description, standard, type)
-# VALUES ('2024-11-27 00:00:00', '2024-11-27 00:00:00', 'FALSE', '', 100, 'CUMULATIVE_LIKES');
-# INSERT IGNORE INTO achievements (created_at, updated_at, is_deleted, description, standard, type)
-# VALUES ('2024-11-27 00:00:00', '2024-11-27 00:00:00', 'FALSE', '', 200, 'CUMULATIVE_LIKES');
-# INSERT IGNORE INTO achievements (created_at, updated_at, is_deleted, description, standard, type)
-# VALUES ('2024-11-27 00:00:00', '2024-11-27 00:00:00', 'FALSE', '', 300, 'CUMULATIVE_LIKES');
-#
-# -- 채택률 업적
-# INSERT IGNORE INTO achievements (created_at, updated_at, is_deleted, description, standard, type)
-# VALUES ('2024-11-27 00:00:00', '2024-11-27 00:00:00', 'FALSE', '', 50, 'ADOPTION_RATE');
-# INSERT IGNORE INTO achievements (created_at, updated_at, is_deleted, description, standard, type)
-# VALUES ('2024-11-27 00:00:00', '2024-11-27 00:00:00', 'FALSE', '', 55, 'ADOPTION_RATE');
-# INSERT IGNORE INTO achievements (created_at, updated_at, is_deleted, description, standard, type)
-# VALUES ('2024-11-27 00:00:00', '2024-11-27 00:00:00', 'FALSE', '', 60, 'ADOPTION_RATE');
-#
-# -- 연속 답변 업적
-# INSERT IGNORE INTO achievements (created_at, updated_at, is_deleted, description, standard, type)
-# VALUES ('2024-11-27 00:00:00', '2024-11-27 00:00:00', 'FALSE', '', 3, 'CONTINUOUS_ANSWER');
-# INSERT IGNORE INTO achievements (created_at, updated_at, is_deleted, description, standard, type)
-# VALUES ('2024-11-27 00:00:00', '2024-11-27 00:00:00', 'FALSE', '', 7, 'CONTINUOUS_ANSWER');
-# INSERT IGNORE INTO achievements (created_at, updated_at, is_deleted, description, standard, type)
-# VALUES ('2024-11-27 00:00:00', '2024-11-27 00:00:00', 'FALSE', '', 14, 'CONTINUOUS_ANSWER');
-#
-# -- 채택 답변 개수 업적
-# INSERT IGNORE INTO achievements (created_at, updated_at, is_deleted, description, standard, type)
-# VALUES ('2024-11-27 00:00:00', '2024-11-27 00:00:00', 'FALSE', '', 10, 'ADOPTION_COUNT');
-# INSERT IGNORE INTO achievements (created_at, updated_at, is_deleted, description, standard, type)
-# VALUES ('2024-11-27 00:00:00', '2024-11-27 00:00:00', 'FALSE', '', 30, 'ADOPTION_COUNT');
-# INSERT IGNORE INTO achievements (created_at, updated_at, is_deleted, description, standard, type)
-# VALUES ('2024-11-27 00:00:00', '2024-11-27 00:00:00', 'FALSE', '', 50, 'ADOPTION_COUNT');
+-- 리뷰 요청 업적
+INSERT IGNORE INTO achievements (created_at, updated_at, is_deleted, description, standard, type)
+VALUES ('2024-11-27 00:00:00', '2024-11-27 00:00:00', 'FALSE', '', 10, 'REQUESTED_REVIEW');
+INSERT IGNORE INTO achievements (created_at, updated_at, is_deleted, description, standard, type)
+VALUES ('2024-11-27 00:00:00', '2024-11-27 00:00:00', 'FALSE', '', 20, 'REQUESTED_REVIEW');
+INSERT IGNORE INTO achievements (created_at, updated_at, is_deleted, description, standard, type)
+VALUES ('2024-11-27 00:00:00', '2024-11-27 00:00:00', 'FALSE', '', 30, 'REQUESTED_REVIEW');
+
+-- 누적 추천 수 업적
+INSERT IGNORE INTO achievements (created_at, updated_at, is_deleted, description, standard, type)
+VALUES ('2024-11-27 00:00:00', '2024-11-27 00:00:00', 'FALSE', '', 100, 'CUMULATIVE_LIKES');
+INSERT IGNORE INTO achievements (created_at, updated_at, is_deleted, description, standard, type)
+VALUES ('2024-11-27 00:00:00', '2024-11-27 00:00:00', 'FALSE', '', 200, 'CUMULATIVE_LIKES');
+INSERT IGNORE INTO achievements (created_at, updated_at, is_deleted, description, standard, type)
+VALUES ('2024-11-27 00:00:00', '2024-11-27 00:00:00', 'FALSE', '', 300, 'CUMULATIVE_LIKES');
+
+-- 채택률 업적
+INSERT IGNORE INTO achievements (created_at, updated_at, is_deleted, description, standard, type)
+VALUES ('2024-11-27 00:00:00', '2024-11-27 00:00:00', 'FALSE', '', 50, 'ADOPTION_RATE');
+INSERT IGNORE INTO achievements (created_at, updated_at, is_deleted, description, standard, type)
+VALUES ('2024-11-27 00:00:00', '2024-11-27 00:00:00', 'FALSE', '', 55, 'ADOPTION_RATE');
+INSERT IGNORE INTO achievements (created_at, updated_at, is_deleted, description, standard, type)
+VALUES ('2024-11-27 00:00:00', '2024-11-27 00:00:00', 'FALSE', '', 60, 'ADOPTION_RATE');
+
+-- 연속 답변 업적
+INSERT IGNORE INTO achievements (created_at, updated_at, is_deleted, description, standard, type)
+VALUES ('2024-11-27 00:00:00', '2024-11-27 00:00:00', 'FALSE', '', 3, 'CONTINUOUS_ANSWER');
+INSERT IGNORE INTO achievements (created_at, updated_at, is_deleted, description, standard, type)
+VALUES ('2024-11-27 00:00:00', '2024-11-27 00:00:00', 'FALSE', '', 7, 'CONTINUOUS_ANSWER');
+INSERT IGNORE INTO achievements (created_at, updated_at, is_deleted, description, standard, type)
+VALUES ('2024-11-27 00:00:00', '2024-11-27 00:00:00', 'FALSE', '', 14, 'CONTINUOUS_ANSWER');
+
+-- 채택 답변 개수 업적
+INSERT IGNORE INTO achievements (created_at, updated_at, is_deleted, description, standard, type)
+VALUES ('2024-11-27 00:00:00', '2024-11-27 00:00:00', 'FALSE', '', 10, 'ADOPTION_COUNT');
+INSERT IGNORE INTO achievements (created_at, updated_at, is_deleted, description, standard, type)
+VALUES ('2024-11-27 00:00:00', '2024-11-27 00:00:00', 'FALSE', '', 30, 'ADOPTION_COUNT');
+INSERT IGNORE INTO achievements (created_at, updated_at, is_deleted, description, standard, type)
+VALUES ('2024-11-27 00:00:00', '2024-11-27 00:00:00', 'FALSE', '', 50, 'ADOPTION_COUNT');
--- wait-for-it.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-host="$1"
-shift
-until mysqladmin ping -h"$host" --silent; do
-  echo "Waiting for MySQL..."
-  sleep 2
-done
-exec "$@"
