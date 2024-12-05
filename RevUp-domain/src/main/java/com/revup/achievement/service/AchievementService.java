94164a138973a190066dacb842772b2d47b7ae82
junho <>
2024-12-05 17:23:55 +0900

    refactor: #48 업적 관련 계산하는 로직 분리

--- RevUp-domain/src/main/java/com/revup/achievement/service/AchievementService.java
+++ RevUp-domain/src/main/java/com/revup/achievement/service/AchievementService.java
@@ -1,13 +1,12 @@
 package com.revup.achievement.service;
 
-import com.revup.achieve.AchieveType;
 import com.revup.achieve.dto.AchievementEventInfo;
 import com.revup.achievement.entity.UserAchievement;
-import com.revup.user.entity.User;
 
 import java.util.List;
 
 public interface AchievementService {
 
     List<UserAchievement> checkAndUpdateAchievement(AchievementEventInfo info);
+
 }
