7aac657316a82a6f9464ad35e940fd0aae880921
junho <>
2024-12-07 17:53:46 +0900

    refactor: #58 프로필 사진 미등록시 기본 프로필사진 자동 저장

--- RevUp-api/src/main/java/com/revup/user/model/request/UpdateProfileRequest.java
+++ RevUp-api/src/main/java/com/revup/user/model/request/UpdateProfileRequest.java
@@ -3,6 +3,8 @@ package com.revup.user.model.request;
 import com.revup.user.entity.Profile;
 import jakarta.validation.constraints.NotEmpty;
 
+import static com.revup.user.constant.UserConstants.BASIC_IMAGE_URL;
+
 public record UpdateProfileRequest(
         @NotEmpty(message = "닉네임은 필수 입력값입니다.")
         String nickname,
@@ -14,9 +16,10 @@ public record UpdateProfileRequest(
 ) {
 
         public Profile toProfile() {
+                String url = profileImage == null ? BASIC_IMAGE_URL : profileImage;
                 return Profile.builder()
                         .nickname(nickname)
-                        .profileImage(profileImage)
+                        .profileImage(url)
                         .introduction(introduction)
                         .build();
         }
