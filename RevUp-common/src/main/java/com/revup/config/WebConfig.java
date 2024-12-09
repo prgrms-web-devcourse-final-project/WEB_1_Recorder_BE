87ee3c53e368156ae6d6e16d735fa5ac0c0f12fa
junho <>
2024-12-09 15:27:07 +0900

    hotfix: #82 cors 경로 수정

--- RevUp-common/src/main/java/com/revup/config/WebConfig.java
+++ RevUp-common/src/main/java/com/revup/config/WebConfig.java
@@ -32,7 +32,7 @@ public class WebConfig implements WebMvcConfigurer {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", githubPageUrl, "https://revup-eight.vercel.app"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
