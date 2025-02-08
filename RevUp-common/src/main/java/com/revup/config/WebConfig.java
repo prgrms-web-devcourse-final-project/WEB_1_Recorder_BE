3034686075c5bd4758d53df77b8ac88d7b64cc5c
junho <>
2025-02-08 20:41:05 +0900

    fix: #174 cors 설정 변경

--- RevUp-common/src/main/java/com/revup/config/WebConfig.java
+++ RevUp-common/src/main/java/com/revup/config/WebConfig.java
@@ -11,7 +11,6 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Configuration
